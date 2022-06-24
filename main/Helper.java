35bd0a80df19e5254a9213e629daa8d843e8e612
Ben Yang <benjyan@amazon.com>
2022-06-24 14:50:55 -0400

    Create project, make simple main

---
+++ main/Helper.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Helper {
+
+    public Helper() {
+
+    }
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+}
---
+++ main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Helper h = new Helper();
+        System.out.println(h.add(2, 2));
+    }
+}
